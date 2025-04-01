7a8234c18b076bc534bd0d3660575578ce812b7e
kenkz447 <kenkz447@gmail.com>
2025-04-01 09:50:14 +0700

    Add DataSheetGrid component with sample data and columns to examples

--- examples/src/pages/example.tsx
+++ examples/src/pages/example.tsx
@@ -1,5 +1,27 @@
+import { checkboxColumn, DataSheetGrid, keyColumn, textColumn } from '@basestacks/react-datagrid';
+import { useState } from 'react';
+
 export default function Example() {
+    const [data, setData] = useState([
+        { active: true, firstName: 'Elon', lastName: 'Musk' },
+        { active: false, firstName: 'Jeff', lastName: 'Bezos' },
+    ]);
+
+    const columns = [
+        { ...keyColumn('active', checkboxColumn), title: 'Active' },
+        { ...keyColumn('firstName', textColumn), title: 'First name' },
+        { ...keyColumn('lastName', textColumn), title: 'Last name' },
+    ];
+
+
     return (
-        <h1>Index</h1>
+        <div>
+            <h1>Index</h1>
+            <DataSheetGrid
+                value={data}
+                onChange={setData as any}
+                columns={columns}
+            />
+        </div>
     );
 };
